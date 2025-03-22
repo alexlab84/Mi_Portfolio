7be7ad355d2f95f2f6ab50310fd47f3e12fa8618
ale <alexlab84@hotmail.com>
2025-03-22 17:17:30 +0100

    Changes in Hasrouter

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Mi_Portfolio/",
-})
+  base: '/Mi_Portfolio/', // 👈 Asegura que coincide con el nombre del repo
+  build: {
+    outDir: 'dist',
+  },
+});
+
